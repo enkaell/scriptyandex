4a8dd75fd8836628a5f63a13231e26350c7865be
Vladislav.Chesnokov <vlad2012chesnokov@mail.ru>
2022-09-16 21:56:27 +0500

    main method

--- main.py
+++ main.py
@@ -6,56 +6,57 @@ from urllib.request import urlopen
 import datetime
 from xml.etree.ElementTree import parse
 
-print('started')
-json_data = {"password": "RAMTRX1500", "regulation": True, "email": "Rakhmanov-2019@list.ru"}
-response = requests.post('https://www.sima-land.ru/api/v5/signin', json=json_data)
-token = response.json().get('token')
-print('token initialized')
-session = requests.Session()
-retry = Retry(connect=2, backoff_factor=0.5)
-adapter = HTTPAdapter(max_retries=retry)
-session.mount('https://', adapter)
-print('trying to get xml...')
-zip = zipfile.ZipFile('ostatki.zip')
-zip.extractall()
-import psutil
-print('trying to parse xml...')
-k = datetime.datetime.now()
 
-xmldoc = parse('t.xml')
-print(psutil.virtual_memory().percent)
-print('starting main loop...')
-print(datetime.datetime.now() - k)
-for tag in xmldoc.iterfind('shop/offers/offer'):
+def main():
+    print('started')
+    json_data = {"password": "RAMTRX1500", "regulation": True, "email": "Rakhmanov-2019@list.ru"}
+    response = requests.post('https://www.sima-land.ru/api/v5/signin', json=json_data)
+    token = response.json().get('token')
+    print('token initialized')
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=1)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    print('trying to get xml...')
+    zip = zipfile.ZipFile('ostatki.zip')
+    zip.extractall()
+    import psutil
+    print('trying to parse xml...')
+    k = datetime.datetime.now()
+    xmldoc = parse('t.xml')
+    print(psutil.virtual_memory().percent)
+    print('starting main loop...')
+    print(datetime.datetime.now() - k)
     try:
-        tag.remove(tag[1])
+        for tag in xmldoc.iterfind('shop/offers/offer'):
+            response = session.get(
+                f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
+                headers={
+                    'accept': 'application/json',
+                    'X-Api-Key': token,
+                    'Authorization': token,
+                },
+
+                params={
+                    'view': 'brief',
+                    'by_sid': 'false',
+                }
+            )
+            print(response.json()['sid'])
+            if int(tag.find('count').text) < 10:
+                tag.find('count').text = '0'
+            else:
+                tag.find('count').text = str(response.json()['balance'])
     except Exception:
-        pass
-    response = session.get(
-        f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
-        headers={
-            'accept': 'application/json',
-            'X-Api-Key': token,
-            'Authorization': token,
-        },
+        xmldoc.write('errors.xml', encoding='utf-8')
+    xmldoc.write('t.xml', encoding='utf-8')
+    zf = zipfile.ZipFile("yandex.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
+    zf.write('t.xml', compresslevel=8)
+    files = {
+        'file': open('yandex.zip', 'rb'),
+    }
+    response = requests.post('https://sima-land-yandex.herokuapp.com/upload', files=files)
+    print(response.json())
 
-        params={
-            'view': 'brief',
-            'by_sid': 'false',
-        }
-    )
-    print(response.json()['sid'])
-    if int(tag.find('count').text) < 10:
-        tag.find('count').text = '0'
-    else:
-        tag.find('count').text = str(response.json()['balance'])
-xmldoc.write('t.xml', encoding='utf-8')
-zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
-zf.write('t.xml', compresslevel=8)
 
-files = {
-    'file': open('ostatki.zip', 'rb'),
-}
-
-response = requests.post('https://give-ur-xml.herokuapp.com/upload', files=files)
-print(response.json())
+main()
